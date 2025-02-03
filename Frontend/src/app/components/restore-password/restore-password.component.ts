12ae0ffdcce8ac4bd5c73f8ff3aca0f46eec0a81
KovagoLevente <kovagolevente@turr.hu>
2025-02-03 20:57:02 +0100

    Emailre érkező megerősítő kód oldal

--- Frontend/src/app/components/restore-password/restore-password.component.ts
+++ Frontend/src/app/components/restore-password/restore-password.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-restore-password',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './restore-password.component.html',
   styleUrl: './restore-password.component.scss'
 })
 export class RestorePasswordComponent {
 
+
+  user = { email: '' }; 
+
+  constructor(
+    private api: ApiService,
+    private auth: AuthService,
+    private router: Router
+  ){}
+
+  restorePw() {
+    console.log('Forgot Password logic for:', this.user.email);
+    alert("új jelszó elmentve");
+    this.router.navigate(["/"]); 
+  }
+  
+    
+  
+    
 }
+
