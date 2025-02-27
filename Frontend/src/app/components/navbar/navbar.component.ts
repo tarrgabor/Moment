b5d942f4572e748de15b5e32fc30b1eaee58ed7d
tarrgabor <80397144+tarrgabor@users.noreply.github.com>
2025-02-27 10:30:34 +0100

    Beállítások popup alap
    Dizájnolni kell még, illetve funkciók backend részét kell megcsinálni

--- Frontend/src/app/components/navbar/navbar.component.ts
+++ Frontend/src/app/components/navbar/navbar.component.ts
@@ -2,14 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { NavItem } from '../../interfaces/interfaces';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent{
+
+  isSettingsOpen = false;
+  activeTab: string = 'profile';
+  confirmationType: 'delete' | 'deactivate' | null = null;
 
   openSidebarMenu()
   {
@@ -21,4 +26,39 @@ export class NavbarComponent{
     document.querySelector(".linksContainer")!.classList.remove("open");
     document.querySelector("body")!.style.overflowY = "auto";
   }
+
+  togglePopup() {
+    this.isSettingsOpen = !this.isSettingsOpen;
+  }
+
+  closePopup() {
+    this.isSettingsOpen = false;
+    this.confirmationType = null;
+  }
+
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
+
+  confirmAction(type: 'delete' | 'deactivate') {
+    this.confirmationType = type;
+  }
+
+  confirm() {
+    if (this.confirmationType === 'delete') {
+      console.log('Fiók törölve');
+    } else if (this.confirmationType === 'deactivate') {
+      console.log('Fiók deaktiválva');
+    }
+    this.closePopup();
+  }
+
+  cancelConfirm() {
+    this.confirmationType = null;
+  }
+
+  toggleTheme() {
+    document.body.classList.toggle('dark-theme');
+  }
+
 }
