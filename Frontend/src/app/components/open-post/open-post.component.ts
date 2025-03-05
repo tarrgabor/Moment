3881c25023ee74be3c63bf09e80679a4bcd6f992
KovagoLevente <kovagolevente@turr.hu>
2025-03-05 18:49:02 +0100

    megnyitott  post  komponens létrehozása

---
+++ Frontend/src/app/components/open-post/open-post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-open-post',
+  imports: [],
+  templateUrl: './open-post.component.html',
+  styleUrl: './open-post.component.scss'
+})
+export class OpenPostComponent {
+
+}
