d69295585b7cb52e06364b8ca71608588a86e9ff
KovagoLevente <kovagolevente@turr.hu>
2025-03-17 21:35:05 +0100

    alap update kinézet kialakítása

---
+++ Frontend/src/app/components/update-post/update-post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+@Component({
+  selector: 'app-update-post',
+  imports: [NavbarComponent],
+  templateUrl: './update-post.component.html',
+  styleUrl: './update-post.component.scss'
+})
+export class UpdatePostComponent {
+
+}
