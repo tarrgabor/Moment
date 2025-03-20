ee87295db21725f8d12de8fe509c5e99ad6098db
tarrgabor <80397144+tarrgabor@users.noreply.github.com>
2025-03-20 12:27:00 +0100

    Beállítások komponensbe rendezése

---
+++ Frontend/src/app/components/profile-update/profile-update.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-update',
+  imports: [],
+  templateUrl: './profile-update.component.html',
+  styleUrl: './profile-update.component.scss'
+})
+export class ProfileUpdateComponent {
+
+}
---
+++ Frontend/src/app/components/theme-change/theme-change.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-change',
+  imports: [],
+  templateUrl: './theme-change.component.html',
+  styleUrl: './theme-change.component.scss'
+})
+export class ThemeChangeComponent {
+
+}
