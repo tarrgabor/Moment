ebf0b125483cd1c54fbec50deb40a34f4b3db879
KovagoLevente <kovagolevente@turr.hu>
2025-04-14 09:04:25 +0200

    alap admin/category/reports komponensek létrehozása

---
+++ Frontend/src/app/components/admin-page/admin-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-page',
+  imports: [],
+  templateUrl: './admin-page.component.html',
+  styleUrl: './admin-page.component.scss'
+})
+export class AdminPageComponent {
+
+}
---
+++ Frontend/src/app/components/category-upload/category-upload.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-upload',
+  imports: [],
+  templateUrl: './category-upload.component.html',
+  styleUrl: './category-upload.component.scss'
+})
+export class CategoryUploadComponent {
+
+}
